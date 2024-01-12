fc2f217e6ee4c86e1ee109f3bc2ac7690c1d38b7
ShevchenkOksana <50699470+ShevchenkOksana@users.noreply.github.com>
2024-01-12 18:23:07 +0200

    Add files via upload

--- src/inference.py
+++ src/inference.py
@@ -1,19 +1,11 @@
 # inference.py
-import os
-from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 
 
-def load_efficientnet_model(model_path, weights_path):
-    model = load_model(model_path)
-    model.load_weights(weights_path)
-    return model
-
-
 def preprocess_image(image_path):
     image = Image.open(image_path)
-    image = image.resize((224, 224))  # Adjust the size based on your model's input size
+    image = image.resize((224, 224))
     image_array = np.array(image) / 255.0
     image_array = np.expand_dims(image_array, axis=0)
     return image_array
